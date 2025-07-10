c559efe79177855847edd4b262295e78cfb85fdc
Fabian Andrade <40735444+FabianAndrade14@users.noreply.github.com>
2025-07-10 18:11:30 -0500

    fix(main): toggle header menu

---
+++ src/assets/js/ScrollReveal.js
@@ -0,0 +1,27 @@
+ScrollReveal().reveal('.about-section h2', {
+    origin: 'left',
+    distance: '40px',
+    duration: 1000,
+    delay: 100
+  });
+
+  ScrollReveal().reveal('.about-section p', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 800,
+    interval: 150
+  });
+
+  ScrollReveal().reveal('.about-photo', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 1000,
+    delay: 400
+  });
+
+  ScrollReveal().reveal('.connect-block', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 1000,
+    delay: 600
+  });
