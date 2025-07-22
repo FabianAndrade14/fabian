d378fa03cedda080a797e2304894ab2d660a079a
Fabian Andrade <40735444+FabianAndrade14@users.noreply.github.com>
2025-07-22 11:17:32 -0500

    fix(main): redirectUri mod

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -10,7 +10,7 @@ export class SpotifyService {
 
   clientId = '5d8cae4ec74e410d9aa291f5d3f72c85';
   redirectUri = 'https://fabian-dev.vercel.app/callback';
-  private accessToken = 'BQBlx2JDP3OLB2Ghw48LbM5_600nUb_PJYnATXzykAQim3SAsmkZGdfzX9wz3Xi5f1k0B5b0TfPlnKtvjW1NXZRvxpQC9RwrM_Hwe5h4bUvQ6ggHA_F1cec6IxpgBjVcLO4z-IAfJub0PPEX5hQHVD8mVKJculi88MRuYn0LI2d7yCQK5a4lxg5Abld8TZJNrwHT93LsRkg37dcZvC3YqSgpU373_qMLQKTSMO1F1wpWLg';
+  private accessToken = 'BQD0k6fAo22kaFSVFCAvcjFfnA8Nz9hmrxY2dUMZ1VO6vBPiicIPp-OsjoRI5aNVTr5NRq5lP3f8TFcCumclOnfUUTVAr6RAV0OApvDKQ3gTLvwabDyoMZYwGHN0kmInjMjeV4i5j8E';
   scopes = [
     'user-read-currently-playing',
     'user-read-recently-played',
