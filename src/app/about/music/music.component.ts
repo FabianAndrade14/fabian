a3768383ec8aa542423734724ea277c31da6e3d7
Fabian Andrade <40735444+FabianAndrade14@users.noreply.github.com>
2025-08-18 17:38:05 -0500

    fix(main): Updated the comic activity

--- src/app/about/music/music.component.ts
+++ src/app/about/music/music.component.ts
@@ -27,8 +27,8 @@ export class MusicComponent implements OnInit {
 
   comicActivity = {
     title: 'Peter Parker The Spectacular Spider-Man',
-    issue: '#10, Volume 1',
-    cover: 'https://cdn.marvel.com/u/prod/marvel/i/mg/3/60/5a06258ec0909/clean.jpg',
+    issue: '#11, Volume 1',
+    cover: 'https://cdn.marvel.com/u/prod/marvel/i/mg/6/10/5a06258c29ad2/clean.jpg',
     progress: 60
   }
 
