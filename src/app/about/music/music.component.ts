13c6559bb41dfbe30ae5493de26e0b5bdd728583
Fabian Andrade <40735444+FabianAndrade14@users.noreply.github.com>
2025-09-30 09:17:45 -0500

    fix(main): Added new comic book cover

--- src/app/about/music/music.component.ts
+++ src/app/about/music/music.component.ts
@@ -27,8 +27,8 @@ export class MusicComponent implements OnInit {
 
   comicActivity = {
     title: 'The Amazing Spider-man',
-    issue: '#22, Volume 6',
-    cover: 'https://cdn.marvel.com/u/prod/marvel/i/mg/3/b0/640905fe64a57/portrait_uncanny.jpg',
+    issue: '#23, Volume 6',
+    cover: 'https://cdn.marvel.com/u/prod/marvel/i/mg/c/60/641e08de5fcd2/portrait_uncanny.jpg',
     progress: 23
   }
 
