4d74866fe802a7562515937fdf219ca59c12dbf2
Fabian Andrade <40735444+FabianAndrade14@users.noreply.github.com>
2025-12-09 23:46:57 -0500

    fix(main): New comicbook cover

--- src/app/about/music/music.component.ts
+++ src/app/about/music/music.component.ts
@@ -28,8 +28,8 @@ export class MusicComponent implements OnInit {
 
   comicActivity = {
     title: 'The Ultimates',
-    issue: '#5, Volume 1',
-    cover: 'https://cdn.marvel.com/u/prod/marvel/i/mg/3/a0/66fc08646155a/portrait_uncanny.jpg',
+    issue: '#8, Volume 1',
+    cover: 'https://cdn.marvel.com/u/prod/marvel/i/mg/4/c0/676445f94d77f/clean.jpg',
     progress: 40
   }
 
