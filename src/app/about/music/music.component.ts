52107bef1a6ded0cdcc7f803caab92efb7d7cc8a
Fabian Andrade <40735444+FabianAndrade14@users.noreply.github.com>
2026-01-11 11:50:02 -0500

    fix(main): new comicbook cover

--- src/app/about/music/music.component.ts
+++ src/app/about/music/music.component.ts
@@ -29,7 +29,7 @@ export class MusicComponent implements OnInit {
   comicActivity = {
     title: 'Nightwing',
     issue: '#9, Volume 4',
-    cover: 'https://static.wikia.nocookie.net/marvel_dc/images/2/22/Nightwing_Vol_4_9_Textless.jpg/revision/latest?cb=20161120022517',
+    cover: 'https://static.dc.com/dc/files/default_images/ntwng_v4_9_5b35751b332cb7.37456426.jpg?w=640',
     progress: 13
   }
 
